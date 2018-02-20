37a2ee2d0323f6d7a5471cf1d5b7514f1fe3cb09
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2018-02-20 17:19:17 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # subtitles-to-elasticsearch
-Takes in an array of XML subtitle files and posts to Elastic Search 
+
+Takes in an array of XML subtitle files and posts to Elastic Search in the following format
+
+```
+{
+  "pid": "123456",
+  "timecode": "543"
+  "text": "some text here"
+}
+```
---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const request = require('request');
+const parser = require('xml2json');
+
+// const subtitlesArray = process.argv[2];
+const subtitleUri = process.argv[2];
+
+// if (!Array.isArray(subtitlesArray)) {
+//   console.log('please provide an array');
+//   return;
+// }
+
+function stripTagsFromString(string) {
+  const stringWithoutTags = string
+    .replace(/\<br \/\>/g, ' ')
+    .replace(/\<\/?span\w+\>/g, '');
+
+  return stringWithoutTags;
+}
+
+// subtitlesArray.forEach((subtitleUri) => {
+  request.get(subtitleUri, (err, res, body) => {
+    if (err) {
+      return console.error('Error', err);
+    }
+
+    const subtitleString = stripTagsFromString(body);
+    const subtitleJson = parser.toJson(subtitleString);
+    const subtitleContents = JSON.parse(subtitleJson).tt.body.div.p;
+
+    console.log(subtitleContents);
+  // });
+});
