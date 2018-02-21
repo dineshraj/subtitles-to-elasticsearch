04e0ea492c4df2732932ee99168698d4c44b64ca
Aimee Rivers <aimee@aimeerivers.com>
2018-02-21 14:45:20 +0000

    Find Episode PID

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ const subtitleUri = process.argv[2];
 //   return;
 // }
 
+const versionPid = /^.+_(\w+)_\d+.xml/.exec(subtitleUri)[1];
+console.log("Version PID: " + versionPid);
+
+request({
+    url: 'http://www.bbc.co.uk/programmes/' + versionPid,
+    followRedirect: false
+  }, (err, res, body) => {
+    let redirectUrl = res.headers.location.split('/');
+    episodePidFound(redirectUrl[redirectUrl.length - 1]);
+  });
+
+function episodePidFound(episodePid) {
+  console.log("Episode PID: " + episodePid);
+}
+
 function stripTagsFromString(string) {
   const stringWithoutTags = string
     .replace(/\<br \/\>/g, ' ')
